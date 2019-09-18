11cb4ea95f66489500589b531658e215978cddfa
kokoliunian <14782340@qq.com>
2019-09-18 13:49:37 +0800

    facenet new thread

---
+++ facedetapp/comparethread.cpp
@@ -0,0 +1,6 @@
+#include "comparethread.h"
+
+CompareThread::CompareThread()
+{
+
+}
---
+++ facedetapp/comparethread.h
@@ -0,0 +1,11 @@
+#ifndef COMPARETHREAD_H
+#define COMPARETHREAD_H
+
+
+class CompareThread : public QThread
+{
+public:
+    CompareThread();
+};
+
+#endif // COMPARETHREAD_H
