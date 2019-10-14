52d8dcf93baef23309afa2afd6eeb6b4341b001c
liud <liud>
2019-10-14 18:49:02 +0800

    update docker build
    更新docker构建文件，主要是针对路径的修改

--- linux_docker_build.sh
+++ linux_docker_build.sh
@@ -1,2 +1,18 @@
+
+#获取当前文件所在路径
+if [ -L $0 ]
+then
+	BASE_DIR=`dirname $(readlink $0)`
+else
+	BASE_DIR=`dirname $0`
+fi
+basepath=$(cd $BASE_DIR; pwd)
+echo $basepath
+
+#进入当前文件所在目录
+cd $basepath
+
 TOP_DIR=`pwd`
-sudo docker run -it -v $TOP_DIR:/app ld3003/v3s_toolchain /bin/bash /app/buildapp.sh
+
+#启动docker编译
+sudo docker run -it -v $TOP_DIR:/$TOP_DIR ld3003/v3s_toolchain /bin/bash $TOP_DIR/buildapp.sh
